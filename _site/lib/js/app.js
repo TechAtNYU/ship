9bb2d99ac21eb91b361d0a14f029c18e94ca4631
Cheryl Wu <grungerabbit@gmail.com>
2013-11-10 21:58:44 -0500

    placeholders

--- _site/lib/js/app.js
+++ _site/lib/js/app.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
 	$("header.main nav li").hover( function() {
-
-		$("header.main").css("background-image", "url(/lib/img/cats/three.jpg)"); },
+		var current = $(this).index();
+		
+		console.log(current)
+		
+		var test;
+		
+		switch(current) {
+			case 0:
+				test = "url(http://subtlepatterns.com/patterns/tree_bark.png)";
+				break;
+			case 1:
+				test = "url(http://subtlepatterns.com/patterns/geometry.png)";
+				break;
+			case 2:
+				test = "url(http://subtlepatterns.com/patterns/knitting250px.png)";
+				break;
+			case 3:
+				test = "url(http://subtlepatterns.com/patterns/shattered.png)";
+				break;
+			case 4:
+				test = "url(http://subtlepatterns.com/patterns/diagonal_striped_brick.png)";
+				break;
+			default:
+				test = "url(/lib/img/cats/one.jpg)";
+				break;
+		}
+		
+		
+		$("header.main").css("background-image", test); },
 		
 		function() { $("header.main").css("background-image", "none");	
 		
